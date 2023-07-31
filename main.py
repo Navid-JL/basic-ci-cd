a9281cb8f02cebf79578d0fb5598fca27d1be464
Navid <navidjalili3@gmail.com>
2023-07-31 23:55:41 +0330

    Add test for Inc

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def greet(name):
     return f"Hello, {name}!"
 
+def inc(x):
+    return x + 1
+
+
 def main():
     name = input("Enter your name: ")
     print(greet(name))
--- test_main.py
+++ test_main.py
@@ -1,4 +1,7 @@
-from main import greet
+from main import greet, inc
 
 def test_greet():
     assert greet("Navid") == "Hello, Navid!"
+    
+def test_answer():
+    assert inc(3) == 5
